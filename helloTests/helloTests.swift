245f79bdcc0b4db59a630572add375a40806e708
Mahamat Ali <mahamat.dev@gmail.com>
2026-02-28 17:11:33 +0100

    Initial Commit

---
+++ helloTests/helloTests.swift
@@ -0,0 +1,17 @@
+//
+//  helloTests.swift
+//  helloTests
+//
+//  Created by Mahamat Ali Youssouf on 28/02/2026.
+//
+
+import Testing
+@testable import hello
+
+struct helloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
